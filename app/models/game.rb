4e9467e9bcdd6a8e4407e94de6c54130ee3a76b1
YankoSoulisimo <yanko.bachvarov@abv.bg>
2014-09-08 11:43:53 +0300

    game and statistic model check

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,9 @@ class Game < ActiveRecord::Base
   belongs_to :user1, class_name: "User"
   belongs_to :user2, class_name: "User"
   belongs_to :word
+  belongs_to :statistic
 
   has_many :moves, dependent: :destroy
-  has_many :statuses, dependent: :destroy
+  has_one :status, dependent: :destroy
   validates :status, numericality: { only_integer: true }
 end
--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,2 +1,3 @@
 class Statistic < ActiveRecord::Base
+  has_many :games
 end
